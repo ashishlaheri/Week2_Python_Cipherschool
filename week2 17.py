cd82d63da901cf35599c5f690b15def7a185efab
Ashish kumar laheri <118184953+ashishlaheri@users.noreply.github.com>
2022-12-12 22:07:37 +0530

    second week

---
+++ week2 17.py
@@ -0,0 +1,31 @@
+# dictonaries intro
+
+# how to create dictonaries
+# step 1
+user = {'name':'Ashish','age': 18}
+# print(user)
+# print(type(user))
+user1=dict(name='Ashish', age= 18)
+# print(user1)
+# how to acess data from dictonary
+# print(user['name'])
+# print(user['age'])
+# which type of data can dictonary store?
+# anything
+
+user_info={
+    'name':'Ashish',
+    'age': 24,
+    'fav_movies': ['coco'],
+    'fav_tunes': ['awakening','fairy tale'],
+
+
+
+
+}
+# print(user_info['fav_tunes'])
+
+# How to add data to empty dictonary
+user_info2= {}
+user_info2['name']='mohit'
+print(user_info2)
---
+++ week2 22.py
@@ -0,0 +1,4 @@
+# more about get method
+user= {'name' : 'Ashish', 'age' : '18', 'age' : '34'}
+# print(user.get('names','not found'))
+print(user)
